01c78d5d7d3eed02d840297083ac62951e1839e9
liulei <121575024@qq.com>
2018-12-07 11:04:16 +0800

    添加jhlabs的imagefilter

---
+++ rpg-core/src/test/java/game/imageoperation/CircleFilterImageOperationFrame.java
@@ -0,0 +1,30 @@
+package game.imageoperation;
+
+import com.jhlabs.image.CircleFilter;
+
+import java.awt.*;
+
+/**
+ * @author lei.liu@datatist.com
+ * @since 2018-12-07 10:48:20
+ */
+public class CircleFilterImageOperationFrame extends ImageOperationBaseFrame {
+
+    public CircleFilterImageOperationFrame(String title) throws HeadlessException {
+        super(title);
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args) {
+        new CircleFilterImageOperationFrame("CircleFilter");
+    }
+
+
+    public void transForm() {
+        CircleFilter circleFilter = new CircleFilter();
+        imageTarget = circleFilter.filter(imageOrig, imageTarget);
+//        imageTarget = ImageUtil.fogImage(imageOrig);
+        repaint();
+    }
+}
---
+++ rpg-thirdparty/README.md
@@ -0,0 +1,3 @@
+该资源来源于互联网：https://download.csdn.net/download/osaymissyou0/9404313，其实在http://www.jhlabs.com/ip/filters/index.html页面中有个download，即可免费下载。
+
+2018/12/7
