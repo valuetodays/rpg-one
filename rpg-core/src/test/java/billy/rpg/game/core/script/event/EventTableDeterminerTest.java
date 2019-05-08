0d382a511f08dfc5d0eba4713d67c7cd6dad1614
liulei <lei.liu@datatist.com>
2019-05-08 12:37:10 +0800

    OPT: tests

--- rpg-core/src/test/java/billy/rpg/game/core/script/event/EventTableDeterminerTest.java
+++ rpg-core/src/test/java/billy/rpg/game/core/script/event/EventTableDeterminerTest.java
@@ -1,5 +1,6 @@
 package billy.rpg.game.core.script.event;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,21 +13,35 @@ public class EventTableDeterminerTest {
 
     @Test
     public void getInstance() {
+        EventTableDeterminer instance = EventTableDeterminer.getInstance();
+        Assert.assertNotNull(instance);
     }
 
     @Test
     public void putEvent() {
+        String event = "aaa";
+        EventTableDeterminer.getInstance().putEvent(event);
+        Assert.assertTrue(EventTableDeterminer.getInstance().existsEvent(event));
     }
 
     @Test
     public void delEvent() {
+        String event = "aaa";
+        EventTableDeterminer.getInstance().putEvent(event);
+        Assert.assertTrue(EventTableDeterminer.getInstance().existsEvent(event));
+        EventTableDeterminer.getInstance().delEvent(event);
+        Assert.assertFalse(EventTableDeterminer.getInstance().existsEvent(event));
     }
 
     @Test
     public void existsEvent() {
+        putEvent();
     }
 
     @Test
     public void printEvents() {
+        String event = "aaa";
+        EventTableDeterminer.getInstance().putEvent(event);
+        EventTableDeterminer.getInstance().printEvents();
     }
 }
